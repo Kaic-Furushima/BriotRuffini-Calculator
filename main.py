f4da00f87e5bb040d74e443b69dfe5659d2bb0e4
Kaic Furushima <kaic.fbs@gmail.com>
2024-07-10 17:08:10 -0300

    Calculate Briot Ruffini function

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+def calculateBR(f_x, function_dividers):
+    roots = []
+    for i in range(len(function_dividers)):
+        calculated_values = []
+        calculated_values.append(f_x[0])
+        print(f"{function_dividers[i]} | ", end="")
+        print(f"{calculated_values[0]} | ", end="")
+        for j in range(1, len(f_x)):
+            calculated_values.append(calculated_values[j - 1] * function_dividers[i] + f_x[j])
+            print(f"{calculated_values[j]} | ", end="")
+        if(calculated_values[len(calculated_values) - 1] == 0):
+            roots.append(calculated_values)
+        print("")
+    print(roots)
+
+
+def buildPanel(f_x):
+    print("***| ", end='')
+    for i in range(len(f_x)):
+        print(f"{f_x[i]} | ", end='')
+
 def getDividers(function_values):
     function_dividers = []
     for i in range(len(function_values)):
@@ -5,8 +26,6 @@ def getDividers(function_values):
             function_dividers.append(i + 1)
             function_dividers.append(-(i + 1))
     return function_dividers
-
-
 
 x = []
 x_value = 'a'
@@ -18,6 +37,7 @@ for i in range(x_degree + 1):
     f_x.append(int(input(f"Insert value {x_value}: ")))
     x_value = chr(ord(x_value) + 1)
 
-function_dividers = getDividers(f_x)
-print(function_dividers)
+buildPanel(f_x)
+print()
+calculateBR(f_x, getDividers(f_x))
 
